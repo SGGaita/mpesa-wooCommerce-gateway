ed9ca59613630e91134dfb73af690aebcd688cc9
Steve Gaita <steveggaita@gmail.com>
2022-05-12 22:39:12 +0300

    add meta to Admin & Invoices from custom form fields in WooCommerce payment gateway description

--- includes/mpesa-checkout-description-fields.php
+++ includes/mpesa-checkout-description-fields.php
@@ -2,6 +2,10 @@
 
 
 add_filter('woocommerce_gateway_description', 'lipa_na_mpesa_description_fields', 20, 2);
+add_action('woocommerce_checkout_process', 'lipa_na_mpesa_description_fields_validation');
+add_action( 'woocommerce_checkout_update_order_meta', 'lipa_na_mpesa_checkout_update_order_meta', 10, 1);
+add_action( 'woocommerce_admin_order_data_after_billing_address', 'lipa_na_mpesa_order_data_after_billing_address', 10, 1);
+add_action( 'woocommerce_order_item_meta_end', 'lipa_na_mpesa_order_item_meta_end', 10, 3);
 
 function lipa_na_mpesa_description_fields($description, $payment_id){
 
@@ -12,7 +16,7 @@ function lipa_na_mpesa_description_fields($description, $payment_id){
     ob_start();
    
     woocommerce_form_field(
-        'phone_number',
+        'payment_number',
         array(
             'type' => 'text',
             'label' =>__('Enter your M-Pesa number', 'mpesa-woo-pay'),
@@ -24,4 +28,24 @@ function lipa_na_mpesa_description_fields($description, $payment_id){
     $description .= ob_get_clean();
     return $description;
 
+}
+
+function lipa_na_mpesa_description_fields_validation(){
+if('mpesa_payment' === $_POST['payment_method'] && ! isset( $_POST['payment_number']) || empty( $_POST['payment_number'])){
+    wc_add_notice( 'Please enter a phone number that is to be billed.', 'error' );
+}
+}
+
+function lipa_na_mpesa_checkout_update_order_meta($order_id){
+    if( isset( $_POST['payment_number']) || ! empty( $_POST['payment_number'])){
+        update_post_meta($order_id, 'payment_number', $_POST[ 'payment_number' ]);
+    }
+}
+
+function lipa_na_mpesa_order_data_after_billing_address( $order ){
+    echo '<p> <strong>'  . __( 'Payment Phone Number: ', 'mpesa-woo-pay' ) . '</strong> <br>' . get_post_meta( $order->get_id(), 'payment_number', true) . '</p>';
+}
+
+function lipa_na_mpesa_order_item_meta_end( $item_id, $item, $order ){
+    echo '<p> <strong>'  . __( 'Payment Phone Number: ', 'mpesa-woo-pay' ) . '</strong> <br>' . get_post_meta( $order->get_id(), 'payment_number', true) . '</p>';
 }
